2304befe8af814705ecd026985cf51da008639c9
Ryan Cook <ryancook423@gmail.com>
2020-05-15 12:16:01 -0400

    Upgraded rollup-plugin-delete 1.1.0 -> 1.2.0

--- packages/ape/dist/index.js
+++ packages/ape/dist/index.js
@@ -5830,7 +5830,7 @@ var APEngineBuildInfo;
      * Version number of the app.
      */
     APEngineBuildInfo.version = '0.1.1';
-    const _time = '1589559215426';
+    const _time = '1589559322526';
     /**
      * The date that this version of the app was built.
      */

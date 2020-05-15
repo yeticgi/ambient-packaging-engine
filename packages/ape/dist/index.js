d2d81e4f49a6d788ab3f2867ff4d4a1e89c5e774
Ryan Cook <ryancook423@gmail.com>
2020-05-15 12:20:49 -0400

    Upgraded tslib 1.10.0 -> 2.0.0

--- packages/ape/dist/index.js
+++ packages/ape/dist/index.js
@@ -5830,7 +5830,7 @@ var APEngineBuildInfo;
      * Version number of the app.
      */
     APEngineBuildInfo.version = '0.1.1';
-    const _time = '1589559452287';
+    const _time = '1589559600056';
     /**
      * The date that this version of the app was built.
      */

36342fb9be34eb14a32f5d8f7b7a08d00c755e33
Ryan Cook <ryancook423@gmail.com>
2021-04-08 15:34:51 -0400

    Upgraded react and react-dom -> 17.0.2

--- packages/ape/dist/index.js
+++ packages/ape/dist/index.js
@@ -3495,7 +3495,7 @@ var APEngineBuildInfo;
      * Version number of the app.
      */
     APEngineBuildInfo.version = '0.5.4';
-    const _time = '1617910209649';
+    const _time = '1617910425428';
     /**
      * The date that this version of the app was built.
      */

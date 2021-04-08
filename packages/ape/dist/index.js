300c9abe4104ebf8b8f7697c2a33171195495d9b
Ryan Cook <ryancook423@gmail.com>
2021-04-08 15:18:55 -0400

    Upgraded `rollup-plugin-sourcemaps` -> 0.6.3

--- packages/ape/dist/index.js
+++ packages/ape/dist/index.js
@@ -3495,7 +3495,7 @@ var APEngineBuildInfo;
      * Version number of the app.
      */
     APEngineBuildInfo.version = '0.5.4';
-    const _time = '1617908405047';
+    const _time = '1617909477046';
     /**
      * The date that this version of the app was built.
      */
